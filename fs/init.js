e7f05f187c6bbeb1c4c401f590d586b33a1eae19
André Lademann <vergissberlin@googlemail.com>
2018-09-19 22:33:45 +0200

    Using tabs!

--- fs/init.js
+++ fs/init.js
@@ -1,10 +1,8 @@
 load('api_config.js');
 load('api_gpio.js');
 load('api_mqtt.js');
-load('api_sys.js');
 load('api_timer.js');
 load("api_esp8266.js");
-load('api_http.js');
 
 
 // ...or set io.adafruit mqtt settings and set feed name here:
