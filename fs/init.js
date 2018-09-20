0d70c68b25bb6abbc2c110cb53ee23024b69c9e9
André Lademann <vergissberlin@googlemail.com>
2018-09-20 22:20:15 +0200

    Remove blink script and add MQTT listener for LED

--- fs/init.js
+++ fs/init.js
@@ -4,21 +4,12 @@ load('api_mqtt.js');
 load('api_timer.js');
 load("api_esp8266.js");
 
-
 // ...or set io.adafruit mqtt settings and set feed name here:
-let feedName = 'coffeebin.sensor';
+let feedSensor = 'coffeebin.sensor';
+let feedLed = 'coffeebin.led';
 
 // Some millisecond settings. adjust to your needs
-let millisToTurnOff = 60000000;
-//***********************************************
-
-// Deep sleep
-/*
-Timer.set(millisToTurnOff , false , function() {
-  print("deep sleep");
-  ESP8266.deepSleep(0);
-}, null);
-*/
+let millisToTurnOff = 60000;
 
 // Configure LED
 let led = 2;
@@ -28,16 +19,9 @@ GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 let button = 0;  
 let pin = 14;  
 
-// Functions
-function blink() {
-  GPIO.write(led,0);
-  print('on');
-  Timer.set(1000, false, function() {
-    print('off');
-    GPIO.write(led,1);
-  }, null);
-}
+//***********************************************
 
+// Functions
 function send(feed, msg, qos) {
 	let topic = Cfg.get('mqtt.user') + '/feeds/' + feed;
 	let ok = MQTT.pub(topic, JSON.stringify(msg||1), qos||1);
@@ -45,28 +29,43 @@ function send(feed, msg, qos) {
 	else print('failed mqtt for' + feed);
 	MQTT.setEventHandler(function(){}, null);
 }
+// Deep sleep
+/*
+Timer.set(millisToTurnOff , false , function() {
+  print("deep sleep");
+  ESP8266.deepSleep(0);
+}, null);
+*/
+
 
 // GPIO listener
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 20, function(x) {
   print('Button press, pin:', x);
-  send(feedName);
+  send(feedSensor);
 }, true);
 GPIO.set_button_handler(pin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 20, function(x) {
   print('Sensor active, pin', x);
-  send(feedName);
+  send(feedSensor);
 }, true);
 
 // MQTT subsriber
-MQTT.sub(Cfg.get('mqtt.user') + '/feeds/' + feedName, function(conn, topic, msg) {
+MQTT.sub(Cfg.get('mqtt.user') + '/feeds/' + feedSensor, function(conn, topic, msg) {
      print('Topic:', topic, 'message:', msg);
-     blink();
  }, null);
+MQTT.sub(Cfg.get('mqtt.user') + '/feeds/' + feedLed, function(conn, topic, msg) {
+     print('Topic:', topic, 'message:', msg);
+    if(JSON.parse(msg) === 0 ){
+      GPIO.write(led, 1);
+    } else {
+     GPIO.write(led, 0);
+    }
+ }, null);
+
 
 // MQTT on booting
 MQTT.setEventHandler(function(conn, ev, edata) {
-	if (ev === MQTT.EV_CONNACK) {
-    send('coffeebin.status');
-	}
+	if (ev === MQTT.EV_CONNACK) send('coffeebin.status')
+  if (ev === MQTT.EV_CLOSE) send('coffeebin.status', 0);
 }, null);
 
 // Info
